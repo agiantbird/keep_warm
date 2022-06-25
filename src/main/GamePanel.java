9eb4bdb41f69fef0ac429157384645ab0555c69c
Rich Hoyt <rhoyt10@gmail.com>
2022-06-25 13:56:37 -0400

    Handle keyboard input and add delta time

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,7 +18,16 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768px
     final int screenHeight = tileSize * maxScreenRow; // 576px
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // set player's default position and velocity
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -26,6 +35,9 @@ public class GamePanel extends JPanel implements Runnable {
         // drawing from this component will be done in an offscreen painting buffer,
         // this can improve the game's rendering performance
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        // make sure panel is 'focused' to receive input
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -35,18 +47,44 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        while(gameThread != null) {
+        double drawInterval = 1_000_000_000.00/FPS; // 0.01666 seconds, so drawing 60FPS
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
-            // 1 UPDATE: update information such as character position
-            update();
-            // 2 DRAW: draw the screen with the updated information
-            // repaint() is how you call paintComponent
-            repaint();
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                // 1 UPDATE: update information such as character position
+                update();
+
+                // 2 DRAW: draw the screen with the updated information
+                // repaint() is how you call paintComponent
+                repaint();
+
+                delta--;
+            }
         }
     }
 
     public void update() {
-
+        if(keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -56,7 +94,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         // dispose this graphics context and release any system resources that it's using
         g2.dispose();
     }

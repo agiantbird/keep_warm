858bf7295577349e56f76b19174bebc94f82651d
Rich Hoyt <rhoyt10@gmail.com>
2022-07-04 14:37:30 -0400

    Paint tiles from text file maps

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,16 +4,24 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum[][];
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
+
         tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+
         getTileImage();
+        loadMap();
     }
 
     public void getTileImage() {
@@ -22,11 +30,42 @@ public class TileManager {
             tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/snow00.png"));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/snow_with_rock_and_stem.png"));
 
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
         } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMap() {
+        try {
+            InputStream is = getClass().getResourceAsStream("/maps/maps01.txt");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+                String line = br.readLine();
+
+                while(col < gp.maxScreenCol) {
+                    String numbers[] = line.split( " ");
+                    // string to int, use col as index for numbers[] array
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col += 1;
+                }
+                if(col == gp.maxScreenCol) {
+                    col = 0;
+                    row +=1;
+                }
+            }
+            br.close();
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
@@ -38,7 +77,9 @@ public class TileManager {
         int y = 0;
 
         while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+
+            int tileNum = mapTileNum[col][row];
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
             col += 1;
             x += gp.tileSize;
 

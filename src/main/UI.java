6464895a8434f0771441e033d6ee3c71138d93c8
Rich Hoyt <rhoyt10@gmail.com>
2022-08-02 09:33:51 -0400

    Add custom font

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,11 +1,14 @@
 package main;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class UI {
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40, arial_80_Bold;
+//    Font arial_40, arial_80_Bold;
+    Font maruMonica, purisaB;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -15,9 +18,18 @@ public class UI {
 
     public UI(GamePanel gp) {
         this.gp = gp;
-
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80_Bold = new Font("Arial", Font.BOLD, 80);
+        // arial_40 = new Font("Arial", Font.PLAIN, 40);
+        // arial_80_Bold = new Font("Arial", Font.BOLD, 80);
+        try {
+            InputStream is = getClass().getResourceAsStream("/font/x12y16pxMaruMonica.ttf");
+            maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+            is = getClass().getResourceAsStream("/font/Purisa_Bold.ttf");
+            purisaB = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void showMessage(String text) {
@@ -28,7 +40,8 @@ public class UI {
     public void draw(Graphics2D g2) {
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+        // g2.setFont(arial_40);
+        g2.setFont(maruMonica);
         g2.setColor(Color.WHITE);
 
         // PLAY STATE
@@ -63,7 +76,7 @@ public class UI {
 
         drawSubWindow(x, y, width, height);
 
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 26F));
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32F));
 
         int dialogueX = x + gp.tileSize;
         int dialogueY = y + gp.tileSize;

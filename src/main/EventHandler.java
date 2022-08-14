96321cfde292528fbe17800aeb579a807b1b9643
Rich Hoyt <rhoyt10@gmail.com>
2022-08-14 11:39:59 -0400

    Refactor and add event conditions

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -5,88 +5,89 @@ import java.awt.*;
 public class EventHandler {
 
     GamePanel gp;
-    Rectangle noKeyInputEventRect;
-    Rectangle keyInputEventRect;
-    int noKeyInputEventRectDefaultX, noKeyInputEventRectDefaultY;
-    int keyInputEventRectDefaultX, keyInputEventRectDefaultY;
+    EventRect eventRect[][];
+
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
-        this.noKeyInputEventRect = new Rectangle();
-        noKeyInputEventRect.x = 23;
-        noKeyInputEventRect.y = 23;
-        noKeyInputEventRect.width = 2;
-        noKeyInputEventRect.height = 2;
-        noKeyInputEventRectDefaultX = noKeyInputEventRect.x;
-        noKeyInputEventRectDefaultY = noKeyInputEventRect.y;
 
-        this.keyInputEventRect = new Rectangle();
-        keyInputEventRect.x = 23;
-        keyInputEventRect.y = 23;
-        keyInputEventRect.width = 48;
-        keyInputEventRect.height = 48;
-        keyInputEventRectDefaultX = keyInputEventRect.x;
-        keyInputEventRectDefaultY = keyInputEventRect.y;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
 
+        int col = 0;
+        int row = 0;
+        while(col < gp.maxWorldCol && row < gp.maxWorldRow) {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+//            eventRect[col][row].width = 2;
+//            eventRect[col][row].height = 2;
+            eventRect[col][row].width = gp.tileSize;
+            eventRect[col][row].height = gp.tileSize;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].x;
+
+            col++;
+            if(col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
+        }
     }
 
     public void checkEvent() {
-        if(noKeyInputEventHit(24, 21, "right")) { sharpIce(gp.dialogueState); }
-        if(keyInputEventHit(25, 21, "any")) { healingTree(gp.dialogueState); }
-        if(keyInputEventHit(25, 20, "down")) { healingTree(gp.dialogueState); }
+
+        // check if player is more than 1 tile away from the last triggered event
+        int xDistance = Math.abs(gp.player.worldX - previousEventX);
+        int yDistance = Math.abs(gp.player.worldY - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        if(distance > gp.tileSize) {
+            canTouchEvent = true;
+        }
+
+        if(canTouchEvent) {
+            // damage event example
+            if(hit(24, 21, "right")) { sharpIce(24, 21, gp.dialogueState); }
+            // healing event example
+            if(hit(25, 20, "down")) { healingTree(25, 20, gp.dialogueState); }
+            if(hit(25, 21, "any")) { healingTree(25, 21, gp.dialogueState); }
+        }
     }
 
-    public boolean noKeyInputEventHit(int eventCol, int eventRow, String reqDirection) {
-        boolean noKeyInputEventHit = false;
+    public boolean hit(int col, int row, String reqDirection) {
+        boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        noKeyInputEventRect.x = eventCol * gp.tileSize + noKeyInputEventRect.x;
-        noKeyInputEventRect.y = eventRow * gp.tileSize + noKeyInputEventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(noKeyInputEventRect)) {
+        if(gp.player.solidArea.intersects(eventRect[col][row]) && !eventRect[col][row].eventDone) {
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
-                noKeyInputEventHit = true;
+                hit = true;
+
+                previousEventX = gp.player.worldX;
+                previousEventY = gp.player.worldY;
             }
         }
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        noKeyInputEventRect.x = noKeyInputEventRectDefaultX;
-        noKeyInputEventRect.y = noKeyInputEventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
-        return noKeyInputEventHit;
+        return hit;
     }
 
-    public boolean keyInputEventHit(int eventCol, int eventRow, String reqDirection) {
-        boolean keyInputEventHit = false;
-
-        gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
-        gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        keyInputEventRect.x = eventCol * gp.tileSize + keyInputEventRect.x;
-        keyInputEventRect.y = eventRow * gp.tileSize + keyInputEventRect.y;
-
-        if(gp.player.solidArea.intersects(keyInputEventRect)) {
-            if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
-                keyInputEventHit = true;
-            }
-        }
-
-        gp.player.solidArea.x = gp.player.solidAreaDefaultX;
-        gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        keyInputEventRect.x = keyInputEventRectDefaultX;
-        keyInputEventRect.y = keyInputEventRectDefaultY;
-
-        return keyInputEventHit;
-    }
-
-    public void sharpIce(int gameState) {
+    public void sharpIce(int col, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You step on sharp ice!";
         gp.player.life -= 1;
+        canTouchEvent = false;
     }
 
-    public void healingTree(int gameState) {
+    public void healingTree(int col, int row, int gameState) {
         if(gp.keyH.enterPressed) {
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You look at this tree. \n...it heals you for some reason?";
